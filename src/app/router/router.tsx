d651cb37751dc8ac6b983405c43992f287bde396
Kostya <paw.dweb@gmail.com>
2024-08-18 21:08:03 +0300

    optimization(lazy-loading)

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Provider } from 'react-redux'
 import { createBrowserRouter } from 'react-router-dom'
 import { store } from 'src/features/store/store'
+import { HomeLazy } from 'src/pages/home/HomeLazy'
+import { ManageLazy } from 'src/pages/management/ManageLazy'
 import Management from 'src/pages/management/Management'
 import Home from '../../pages/home/Home'
 import { App } from '../App'
@@ -17,11 +19,19 @@ export const router = createBrowserRouter([
 		children: [
 			{
 				path: '/management',
-				element: <Management />
+				element: (
+					<Suspense fallback={<h1>Загрузка...</h1>}>
+						<ManageLazy />
+					</Suspense>
+				)
 			},
 			{
 				path: '/',
-				element: <Home />
+				element: (
+					<Suspense fallback={<h1>Загрузка...</h1>}>
+						<HomeLazy />
+					</Suspense>
+				)
 			}
 		]
 	}
---
+++ src/pages/management/ManageLazy.ts
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export const ManageLazy = React.lazy(() => import('./Management'))
